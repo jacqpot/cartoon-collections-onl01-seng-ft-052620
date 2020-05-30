3fb02232e974e2f833da335fe094a4211a532056
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-30 01:30:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  order = []
+  names.map.with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(array)
+  array.map! {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+          # cheese = ""
+          # cheese_types = ["cheddar", "gouda", "camembert"]
+          # if cheese << array.any? == cheese_types 
+          #   puts cheese
+          # else
+          # end
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{ |ingredient| ingredient.include?(cheese_types) "#{ingredient}"
+
 end
