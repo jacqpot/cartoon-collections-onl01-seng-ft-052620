1ae0d66430cd5ed7a5497c04184d9bdf4d706fdb
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-30 01:32:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,12 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-          # cheese = ""
-          # cheese_types = ["cheddar", "gouda", "camembert"]
-          # if cheese << array.any? == cheese_types 
-          #   puts cheese
-          # else
-          # end
+
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find{ |ingredient| (cheese_types).include?(ingredient)}
 
